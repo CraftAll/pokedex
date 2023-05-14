3b8aab6805c134f1e108502f328f3ea99c4af9f8
CraftAll <robert.macionczyk@gmail.com>
2023-05-14 22:56:56 +0200

    Add scss

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import { router } from "./router";
 import { RouterProvider } from "react-router-dom";
+import "./index.scss";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
